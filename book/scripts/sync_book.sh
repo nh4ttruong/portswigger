008de883a97eac9c1ee5c2b8b5c4417b12b9a167
Nhật Trường <truongtbn243@gmail.com>
2025-12-07 16:49:22 +0000

    enhance: ci

---
+++ book/scripts/sync_book.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# List of all directories to be copied
+directories=(
+    "file-upload"
+    "oauth"
+    "sql-injection"
+    "xss"
+)
+
+cd $(git rev-parse --show-toplevel)
+
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "Copying '$dir' to 'book/src/$dir'"
+        mkdir -p "book/src/$dir"
+        cp -r "$dir/"* "book/src/$dir/"
+    elif [ -f "$dir" ]; then
+        echo "Copying '$dir' to 'book/src/$dir'"
+        cp "$dir" "book/src/$dir"
+    else
+        echo "Directory $dir does not exist, skipping."
+    fi
+done
